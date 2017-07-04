6ca3d6515e54d558307220702a569f2a6d4dc58a
NeverTwice <samuelantunes@hotmail.fr>
2017-07-04 19:31:25 +0200

    SQLite configuration

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "sqlite3.h"
 
-int main(int argc, char** argv)
-{
-    printf("Hello world!\n");
-    return 0;
+int main(int argc, char** argv) {
+   sqlite3 *db;
+   char *zErrMsg = 0;
+   int rc;
+
+   rc = sqlite3_open("LaBanque.db", &db);
+
+   if( rc ) {
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      return(0);
+   } else {
+      fprintf(stderr, "Opened database successfully\n");
+   }
+   sqlite3_close(db);
 }
